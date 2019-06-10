6e49745cb2ea076a97166e6771a75b9d41a4ee81
Slah-Sani <sanislah@gmail.com>
2019-06-11 00:19:01 +0300

    more changes

--- fun.py
+++ fun.py
@@ -2,4 +2,6 @@ fun = input("Do you know basics of git: ")
 if fun == "yes":
     print("great")
 else:
-    print("let's practice more. ")
+    if fun == "no":
+         print("let's practice more. ")
+         
